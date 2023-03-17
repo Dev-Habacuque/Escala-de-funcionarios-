3d7f863490fea310a97af4a02f35c4a361520f87
jeremias habacuque <jeremiashabacuque321@gmail.com>
2023-03-16 21:10:48 -0300

    Alterei a escrita no input

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 
-let jeremias = ['terça','quinta']
-let sara = ['quinta','sabado']
-let ezequiel = ['segunda','quarta']
-let lisa = ['sexta','domingo']
+let jeremias = ['Terça','Quinta']
+let sara = ['Quinta','Sábado']
+let ezequiel = ['Segunda','Quarta']
+let lisa = ['Sexta','Domingo']
 
 document.getElementById('bott').addEventListener('click', func)
 
@@ -12,22 +12,22 @@ function func(){
 let dia = document.getElementById('dia').value
 
 if (dia == 'segunda'){
-    document.getElementById('caixa').value = 'Ezequiel lava segunda'
+    document.getElementById('caixa').value = 'Ezequiel lava segunda-feira'
 }
 else if(dia == 'terça'){
-    document.getElementById('caixa').value = 'Jeremias lava terça'
+    document.getElementById('caixa').value = 'Jeremias lava terça-feira'
 }
 else if(dia == 'quarta'){
-    document.getElementById('caixa').value = ' Ezequiel lava quarta'
+    document.getElementById('caixa').value = ' Ezequiel lava quarta-feira'
 }
 else if (dia == 'quinta'){
-    document.getElementById('caixa').value = 'Jeremias lava quinta'
+    document.getElementById('caixa').value = 'Jeremias lava quinta-feira'
 }
 else if(dia == 'sexta'){
-    document.getElementById('caixa').value = 'Lisa lava sexta'
+    document.getElementById('caixa').value = 'Lisa lava sexta-feira'
 }
 else if(dia == 'sabado'){
-    document.getElementById('caixa').value = 'Sara lava sabado'
+    document.getElementById('caixa').value = 'Sara lava sábado'
 }
 else{
     document.getElementById('caixa').value = 'Lisa lava domingo'  
